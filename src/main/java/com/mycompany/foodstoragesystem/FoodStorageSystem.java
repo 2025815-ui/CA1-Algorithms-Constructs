ded9c990297e9c4cfbe55a5ea34ea85e27af8dc8
2025815-ui <2025815@student.cct.ie>
2026-03-30 22:14:40 +0100

    Initial commit - Adding assignment folder and created Main class

---
+++ src/main/java/com/mycompany/foodstoragesystem/FoodStorageSystem.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.foodstoragesystem;
+import java.util.Scanner;
+/**
+ *
+ * @author yarzarmin
+ */
+public class FoodStorageSystem {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("\n=======MENU=======");
+            System.out.println("1. Add Food");
+            System.out.println("2. Remove Food from Front");
+            System.out.println("3. Remove Food from Rear");
+            System.out.println("4. Peek Top Item in storage");
+            System.out.println("5. Display all items in storage");
+            System.out.println("6. Exit the program");
+            
+            int choice = input.nextInt();
+            
+            
+            
+        }
+    }
+}
